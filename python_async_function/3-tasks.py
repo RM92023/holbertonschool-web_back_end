b3d98ff9cbbe513bb1d85d87957635a84b8aa13d
RM92023 <robinjara20@gmail.com>
2023-08-31 10:53:35 -0500

    update code

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -11,8 +11,3 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Tasks """
     task = asyncio.create_task(wait_random(max_delay))
     return task
-
-    Create a measure_time function with integers n and max_delay as arguments
-    that measures the total execution time for wait_n(n, max_delay), and
-    returns total_time / n. Your function should return a float.
-    Use the time module to measure an approximate elapsed time.
