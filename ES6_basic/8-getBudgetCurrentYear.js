b5cb52f599f04e06fef1eab920c02aec0feb9c61
RM92023 <robinjara20@gmail.com>
2023-09-13 15:37:31 -0500

    adding new year in code

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+function getCurrentYear() {
+  const currentYear = 2021;
+  return currentYear;
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
