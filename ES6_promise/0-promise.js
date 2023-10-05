6a33a91ca84cbe0e2eed79021961e8f02e1ce0e4
RM92023 <robinjara20@gmail.com>
2023-10-05 10:15:48 -0500

    update code because lint is mandatory use the resolve and reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    const rm = true;
+    if (rm) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
